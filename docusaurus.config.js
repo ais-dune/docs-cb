5a33cbbbbff8785a2fa16258751cdf3d94dc1d3c
ais-dune <duneforestlab@gmail.com>
2025-07-30 18:32:57 +0900

    mod: config 수정

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -72,7 +72,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/ais-dune/docs-cb/edit/main/',
         },
         blog: {
           showReadingTime: true,
@@ -83,7 +83,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/ais-dune/docs-cb/edit/main/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
